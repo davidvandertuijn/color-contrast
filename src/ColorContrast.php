66cfe33559328bfc4799ed83b6673b93fa0cc271
David van der Tuijn <david@digitalimpact.nl>
2022-03-06 23:03:53 +0100

    Code Optimization

--- src/ColorContrast.php
+++ src/ColorContrast.php
@@ -2,46 +2,49 @@
 
 namespace Davidvandertuijn;
 
+/**
+ * Color Contrast.
+ */
 class ColorContrast
 {
     /**
      * Parse.
      *
-     * @param string $sColor
+     * @param string $color
      *
      * @return string
      */
-    public static function parse(string $sColor): string {
+    public static function parse(string $color): string {
         // HEX to RGB
 
-        $R1 = hexdec(substr($sColor, 1, 2));
-        $G1 = hexdec(substr($sColor, 3, 2));
-        $B1 = hexdec(substr($sColor, 5, 2));
+        $r1 = hexdec(substr($color, 1, 2));
+        $g1 = hexdec(substr($color, 3, 2));
+        $b1 = hexdec(substr($color, 5, 2));
 
         // Black
 
-        $sBlack = '#000000';
-        $R2Black = hexdec(substr($sBlack, 1, 2));
-        $G2Black = hexdec(substr($sBlack, 3, 2));
-        $B2Black = hexdec(substr($sBlack, 5, 2));
+        $black = '#000000';
+        $r2black = hexdec(substr($black, 1, 2));
+        $g2black = hexdec(substr($black, 3, 2));
+        $b2black = hexdec(substr($black, 5, 2));
 
         // Contrast Ratio
 
-        $L1 = 0.2126 * pow($R1 / 255, 2.2) +
-            0.7152 * pow($G1 / 255, 2.2) +
-            0.0722 * pow($B1 / 255, 2.2);
+        $l1 = 0.2126 * pow($r1 / 255, 2.2) +
+            0.7152 * pow($g1 / 255, 2.2) +
+            0.0722 * pow($b1 / 255, 2.2);
 
-        $L2 = 0.2126 * pow($R2Black / 255, 2.2) +
-            0.7152 * pow($G2Black / 255, 2.2) +
-            0.0722 * pow($B2Black / 255, 2.2);
+        $l2 = 0.2126 * pow($r2black / 255, 2.2) +
+            0.7152 * pow($g2black / 255, 2.2) +
+            0.0722 * pow($b2black / 255, 2.2);
 
-        if ($L1 > $L2) {
-            $iContrastRatio = (int) (($L1 + 0.05) / ($L2 + 0.05));
+        if ($l1 > $l2) {
+            $contrastRatio = (int) (($l1 + 0.05) / ($l2 + 0.05));
         } else {
-            $iContrastRatio = (int) (($L2 + 0.05) / ($L1 + 0.05));
+            $contrastRatio = (int) (($l2 + 0.05) / ($l1 + 0.05));
         }
 
-        if ($iContrastRatio > 5) {
+        if ($contrastRatio > 5) {
             return '#000000';
         } else {
             return '#FFFFFF';
